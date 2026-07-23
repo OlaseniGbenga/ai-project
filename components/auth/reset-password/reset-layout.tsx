c3d263584334198af342c7306d91ef65a4b5eaae
Gbenga Olaseni <gj.olaseni@gmail.com>
2026-07-23 21:31:40 +0100

    done

--- components/auth/reset-password/reset-layout.tsx
+++ components/auth/reset-password/reset-layout.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import Image from "next/image";
-// import BackButton from "@/components/ui/btn/back-button";
+import React from "react";
+import { Stack, Text } from "@mantine/core";
 
 type ResetLayoutProps = {
   children: React.ReactNode;
@@ -9,18 +7,13 @@ type ResetLayoutProps = {
 
 export default function ResetLayout({ children }: ResetLayoutProps) {
   return (
-    // <div className=" w-full ">
-    //   <div className="">
-    //     <BackButton />
-    //   </div>
     <div className="flex flex-col items-center justify-center gap-6 w-full px-4">
-      <Image
-        src="/logo.svg"
-        alt="Logo"
-        width={100}
-        height={100}
-        className="self-center mb-8"
-      />
+      <Text fw={800} size="32px" c="#066F33">
+        CUNIV AI
+      </Text>
+      <Text size="16px" py={2} c="#8E8E8E">
+        Learn, apply, grow
+      </Text>
       {children}
     </div>
     // </div>
